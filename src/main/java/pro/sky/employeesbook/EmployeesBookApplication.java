a8b8ba147feda1cda530b310243d2742dd06380f
Timur <mirtimoha@gmail.com>
2023-04-14 10:15:06 +0500

    first commit

---
+++ src/main/java/pro/sky/employeesbook/EmployeesBookApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employeesbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeesBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/employeesbook/EmployeesBookApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.employeesbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
